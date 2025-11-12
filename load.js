29c69819943d9f3f8145774cd5001a0d7a2c8639
hl <87237417+brianpap@users.noreply.github.com>
2025-11-12 17:55:28 -0600

    Update load.js

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
 let page = window.location.href
-fetch("")
+fetch("https://raw.githubusercontent.com/brianpap/codeInsert/refs/heads/main/load.js")
   .then(e=>{return e.text()}
   .then(data=>{document.getElementsByTagName("html")[0].outerHTML=data)
