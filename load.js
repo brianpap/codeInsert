8a0c3e9183efbb2324fe3ddf10c66db9d8bf1219
hl <87237417+brianpap@users.noreply.github.com>
2025-11-12 18:08:24 -0600

    Update and rename load.js to load.html

--- load.js
+++
@@ -1,4 +0,0 @@
-let page = window.location.href
-fetch("https://raw.githubusercontent.com/brianpap/codeInsert/refs/heads/main/load.js")
-  .then(e=>{return e.text()}
-  .then(data=>{document.getElementsByTagName("html")[0].outerHTML=data)
